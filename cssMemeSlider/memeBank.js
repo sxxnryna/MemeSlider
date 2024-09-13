fe9992d6d95389e8a604348b2f7f82edcbd64e1e
Ryna <katrin_dr@naver.com>
2024-09-13 22:18:33 +0900

    refactor: make some meme pics changes (Friday, Sept 13, 2024, 10:18:35)

---
+++ cssMemeSlider/memeBank.js
@@ -0,0 +1,22 @@
+export const meme = [
+  {
+    id: "01",
+    img: "./assets/sub-buzz.png",
+    description: "Probably everybody knows the feeling",
+  },
+  {
+    id: "02",
+    img: "./assets/sleep-memes.png",
+    description: "I feel that hurts",
+  },
+  {
+    id: "03",
+    img: "./assets/dont-touch.png",
+    description: "No touchy-touchy",
+  },
+  {
+    id: "04",
+    img: "./assets/meme-exhibition.png",
+    description: "Very interesting!",
+  },
+];
